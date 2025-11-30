e88c80c92d38b5484acf890253e3021068ab200b
CryJoyYarn <708994182@qq.com>
2025-11-30 14:03:26 +0800

    修99a 修j10

--- src/main/java/com/redabysslucia/dragonrise_reforge/init/ModEntityRenderers.java
+++ src/main/java/com/redabysslucia/dragonrise_reforge/init/ModEntityRenderers.java
@@ -3,7 +3,7 @@ package com.redabysslucia.dragonrise_reforge.init;
 import com.redabysslucia.dragonrise_reforge.client.renderer.entity.J10Renderer;
 import com.redabysslucia.dragonrise_reforge.client.renderer.entity.KA50Renderer;
 import com.redabysslucia.dragonrise_reforge.client.renderer.entity.T80Renderer;
-import com.redabysslucia.dragonrise_reforge.client.renderer.entity.ZTZ99aRenderer;
+import com.redabysslucia.dragonrise_reforge.client.renderer.entity.ZTZ99ARenderer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.Mod;
 public class ModEntityRenderers {
         @SubscribeEvent
         public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event){
-                event.registerEntityRenderer(ModEntities.ZTZ99A.get(), ZTZ99aRenderer::new);
+                event.registerEntityRenderer(ModEntities.ZTZ99A.get(), ZTZ99ARenderer::new);
                 event.registerEntityRenderer(ModEntities.KA50.get(), KA50Renderer::new);
                 event.registerEntityRenderer(ModEntities.T80.get(), T80Renderer::new);
                 event.registerEntityRenderer(ModEntities.J10.get(), J10Renderer::new);
