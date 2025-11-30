45537044bbf2656982d55c08514f4b768e451736
Ly01_lin <863716833@qq.com>
2025-11-30 19:53:56 +0800

    修复性提交

--- src/main/java/com/redabysslucia/dragonrise_reforge/init/ModEntities.java
+++ src/main/java/com/redabysslucia/dragonrise_reforge/init/ModEntities.java
@@ -21,6 +21,22 @@ public class ModEntities {
                         .sized(4.0f, 2.9f)
         );
 
+       public static final RegistryObject<EntityType<ZTZ99BHEntity>> ZTZ99BH = register("ztz99bh",
+               EntityType.Builder.<ZTZ99BHEntity>of(ZTZ99BHEntity::new, MobCategory.MISC)
+                       .setTrackingRange(512)
+                       .setUpdateInterval(1)
+                       .fireImmune()
+                       .sized(4.0f, 2.9f)
+       );
+
+      public static final RegistryObject<EntityType<M1A2SEPV2Entity>> M1A2SEPV2 = register("m1a2sepv2",
+              EntityType.Builder.<M1A2SEPV2Entity>of(M1A2SEPV2Entity::new, MobCategory.MISC)
+                      .setTrackingRange(512)
+                      .setUpdateInterval(1)
+                      .fireImmune()
+                      .sized(4.0f, 2.9f)
+      );
+
         public static final RegistryObject<EntityType<KA50Entity>> KA50 = register("ka50",
                 EntityType.Builder.<KA50Entity>of(KA50Entity::new, MobCategory.MISC)
                         .setTrackingRange(512)
