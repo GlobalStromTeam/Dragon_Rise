6099f5e5a3cec358566b99949dede09c3d3c2bbd
Ly01_lin <863716833@qq.com>
2025-11-30 20:35:40 +0800

    中文语言修正，创造物品栏修正

--- src/main/java/com/redabysslucia/dragonrise_reforge/init/ModTabs.java
+++ src/main/java/com/redabysslucia/dragonrise_reforge/init/ModTabs.java
@@ -14,18 +14,20 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModTabs {
         public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Dragonrise_reforge.MODID);
 
-        public static final RegistryObject<CreativeModeTab> BLOCK_TAB = TABS.register("dragonrise_reforge",
+        public static final RegistryObject<CreativeModeTab> MBT_TAB = TABS.register("dragonrise_reforge",
                 () -> CreativeModeTab.builder()
                         .title(Component.translatable("item_group.dragonrise_reforge.title"))
                         .icon(() -> new ItemStack(ModItems.CONTAINER.get()))
                         .displayItems((param, output) -> {
                             output.accept(ContainerBlockItem.createInstance(ModEntities.ZTZ99A.get()));
-                            output.accept(ContainerBlockItem.createInstance(ModEntities.KA50.get()));
-                            output.accept(ContainerBlockItem.createInstance(ModEntities.T80.get()));
-                            output.accept(ContainerBlockItem.createInstance(ModEntities.AA625E.get()));
-                            output.accept(ContainerBlockItem.createInstance(ModEntities.J10.get()));
-                            output.accept(ContainerBlockItem.createInstance(ModEntities.Z10ME.get()));
+                            output.accept(ContainerBlockItem.createInstance(ModEntities.ZTZ99BH.get()));
                             output.accept(ContainerBlockItem.createInstance(ModEntities.ZBL08.get()));
+                            output.accept(ContainerBlockItem.createInstance(ModEntities.AA625E.get()));
+                            output.accept(ContainerBlockItem.createInstance(ModEntities.Z10ME.get()));
+                            output.accept(ContainerBlockItem.createInstance(ModEntities.J10.get()));
+                            output.accept(ContainerBlockItem.createInstance(ModEntities.T80.get()));
+                            output.accept(ContainerBlockItem.createInstance(ModEntities.KA50.get()));
+                            output.accept(ContainerBlockItem.createInstance(ModEntities.M1A2SEPV2.get()));
                                 }
                         ).build()
         );
