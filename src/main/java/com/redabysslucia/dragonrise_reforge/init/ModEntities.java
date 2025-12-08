47e9f75fd3adb6473d5abd1406b5e6370cbcb63a
Ly01_lin <863716833@qq.com>
2025-12-08 23:43:57 +0800

    修复q5

--- src/main/java/com/redabysslucia/dragonrise_reforge/init/ModEntities.java
+++ src/main/java/com/redabysslucia/dragonrise_reforge/init/ModEntities.java
@@ -157,6 +157,22 @@ public class ModEntities {
                     .sized(4.0f, 2.7f)
     );
 
+    public static final RegistryObject<EntityType<J20Entity>> J20 = register("j20",
+            EntityType.Builder.of(J20Entity::new, MobCategory.MISC)
+                    .setTrackingRange(512)
+                    .setUpdateInterval(2)
+                    .fireImmune()
+                    .sized(4.0f, 2.7f)
+    );
+
+    public static final RegistryObject<EntityType<Q5Entity>> Q5 = register("q5",
+            EntityType.Builder.of(Q5Entity::new, MobCategory.MISC)
+                    .setTrackingRange(512)
+                    .setUpdateInterval(2)
+                    .fireImmune()
+                    .sized(4.0f, 2.7f)
+    );
+
 
 
         private static <T extends Entity> RegistryObject<EntityType<T>> register(String name, EntityType.Builder<T> entityTypeBuilder) {
