e8037792ff4984418233fb69df37f37a9b076a33
Lg <16448113+lgen141@user.noreply.gitee.com>
2025-12-10 02:11:24 +0800

    j20发光层

--- src/main/java/com/redabysslucia/dragonrise_reforge/client/renderer/entity/J20Renderer.java
+++ src/main/java/com/redabysslucia/dragonrise_reforge/client/renderer/entity/J20Renderer.java
@@ -1,14 +1,14 @@
 package com.redabysslucia.dragonrise_reforge.client.renderer.entity;
 
-import com.atsuishio.superbwarfare.client.renderer.entity.VehicleRenderer;
-import com.redabysslucia.dragonrise_reforge.client.model.entity.J10Model;
+import com.redabysslucia.dragonrise_reforge.client.layer.J20GlowLayer;
 import com.redabysslucia.dragonrise_reforge.client.model.entity.J20Model;
-import com.redabysslucia.dragonrise_reforge.entities.J10Entity;
 import com.redabysslucia.dragonrise_reforge.entities.J20Entity;
+import com.atsuishio.superbwarfare.client.renderer.entity.VehicleRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 
 public class J20Renderer extends VehicleRenderer<J20Entity> {
         public J20Renderer(EntityRendererProvider.Context renderManager) {
-                super(renderManager,new J20Model());
+                super(renderManager, new J20Model());
+                this.addRenderLayer(new J20GlowLayer(this));
         }
 }
