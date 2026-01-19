8b58eec62c7553126690152ff901a1958aa4e7d6
Ly01_lin <863716833@qq.com>
2026-01-19 20:13:38 +0800

    怎么老是有东西没进版本管理

---
+++ src/main/java/com/redabysslucia/dragonrise_reforge/entities/JF17Entity.java
@@ -0,0 +1,66 @@
+package com.redabysslucia.dragonrise_reforge.entities;
+
+import com.atsuishio.superbwarfare.entity.vehicle.base.GeoVehicleEntity;
+import com.atsuishio.superbwarfare.entity.vehicle.damage.DamageModifier;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.core.animation.AnimationController;
+import software.bernie.geckolib.core.animation.AnimationState;
+import software.bernie.geckolib.core.animation.RawAnimation;
+import software.bernie.geckolib.core.object.PlayState;
+
+import static com.atsuishio.superbwarfare.event.ClientEventHandler.zoomVehicle;
+
+public class JF17Entity extends GeoVehicleEntity {
+
+    public JF17Entity(EntityType<JF17Entity> type, Level world) {
+        super(type, world);
+    }
+
+    @Override
+    public DamageModifier getDamageModifier() {
+        return super.getDamageModifier()
+                .custom((source, damage) -> getSourceAngle(source, 0.25f) * damage * (getHealth() > 0.1f ? 0.4f : 0.05f));
+    }
+
+    private PlayState Sb(AnimationState<JF17Entity> event) {
+        if (this.onGround()) {
+            return event.setAndContinue(RawAnimation.begin().thenPlayAndHold
+                    ("起落架放"));
+        }
+        else {
+        return event.setAndContinue(RawAnimation.begin().thenPlayAndHold
+                ("起落架收"));
+        }
+    }
+
+    private PlayState Sb1(AnimationState<JF17Entity> event) {
+        if (this.sprintInputDown()) {
+            return event.setAndContinue(RawAnimation.begin().thenPlayAndHold
+                    ("engine on"));
+        }
+        else {
+            return event.setAndContinue(RawAnimation.begin().thenPlayAndHold
+                    ("engine off"));
+        }
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar data) {
+        data.add(new AnimationController<>(this, "sb", 0, this::Sb));
+        data.add(new AnimationController<>(this, "sb1", 0, this::Sb1));
+    }
+
+
+
+//    @Override
+//    public boolean useAircraftCamera(int seatIndex) {
+//        return ModKeyMappings.FREE_CAMERA.isDown() && !ClientEventHandler.zoom;
+//    }
+
+    @Override
+    public double getMouseSensitivity() {
+        return zoomVehicle ? 0.03 : 0.07;
+    }
+}
