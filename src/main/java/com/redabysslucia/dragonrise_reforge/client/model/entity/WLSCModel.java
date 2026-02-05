e19c9653df6db01baa386df7bb3a3a33a7ff020b
Lg <16448113+lgen141@user.noreply.gitee.com>
2026-02-06 04:59:33 +0800

    声音

--- src/main/java/com/redabysslucia/dragonrise_reforge/client/model/entity/WLSCModel.java
+++ src/main/java/com/redabysslucia/dragonrise_reforge/client/model/entity/WLSCModel.java
@@ -1,7 +1,6 @@
 package com.redabysslucia.dragonrise_reforge.client.model.entity;
 
 import com.atsuishio.superbwarfare.client.model.entity.VehicleModel;
-import com.redabysslucia.dragonrise_reforge.entities.TOYOTASEIKIEntity;
 import com.redabysslucia.dragonrise_reforge.entities.WLSCEntity;
 
 public class WLSCModel extends VehicleModel<WLSCEntity> {
