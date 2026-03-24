932e751ae00faf1bb9f14d59e9f8f8fb47466a09
Lg <16448113+lgen141@user.noreply.gitee.com>
2026-03-25 07:21:50 +0800

    开火发光和引擎烟

--- src/main/java/com/redabysslucia/dragonrise_reforge/entities/T80Entity.java
+++ src/main/java/com/redabysslucia/dragonrise_reforge/entities/T80Entity.java
@@ -2,6 +2,8 @@ package com.redabysslucia.dragonrise_reforge.entities;
 
 import com.atsuishio.superbwarfare.entity.vehicle.damage.DamageModifier;
 import com.redabysslucia.dragonrise_reforge.entities.utils.NightVisionVehicle;
+import com.redabysslucia.dragonrise_reforge.utils.EngineParticleUtil;
+import com.redabysslucia.dragonrise_reforge.utils.TurretLightUtil;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
@@ -13,29 +15,47 @@ import software.bernie.geckolib.core.object.PlayState;
 @SuppressWarnings("removal")
 public class T80Entity extends NightVisionVehicle {
 
-        public T80Entity(EntityType<?> pEntityType, Level pLevel) {
-                super(pEntityType, pLevel);
-        }
+	public T80Entity(EntityType<?> pEntityType, Level pLevel) {
+		super(pEntityType, pLevel);
+	}
 
-        @Override
-        public DamageModifier getDamageModifier() {
-                return super.getDamageModifier()
-                        .custom((source, damage) -> getSourceAngle(source, 0.3f) * damage);
-        }
-        private PlayState cannonFirePredicate(AnimationState<T80Entity> event) {
-                if (getShootAnimationTimer(0, 0) > 0) {
-                        return event.setAndContinue(RawAnimation.begin().thenPlayAndHold("animation.t80.fire"));
-                }
-                return event.setAndContinue(RawAnimation.begin().thenLoop("animation.t80.idle"));
-        }
+	@Override
+	public DamageModifier getDamageModifier() {
+		return super.getDamageModifier()
+				.custom((source, damage) -> getSourceAngle(source, 0.3f) * damage);
+	}
 
-        @Override
-        public void registerControllers(AnimatableManager.ControllerRegistrar data) {
-                data.add(new AnimationController<>(this, "cannon", 0, this::cannonFirePredicate));
-        }
+	// 用于跟踪需要删除的光源方块
+	private int lightRemovalTimer = 0;
 
-        @Override
-        public ResourceLocation getNightVisionShader() {
-                return new ResourceLocation("shaders/post/night-vision-wp.json");
-        }
+	@Override
+	public void tick() {
+		super.tick();
+		if (tickCount % 5 == 0) {
+			// 在MainEngine obb框位置生成粒子效果
+			EngineParticleUtil.spawnMainEngineParticles(this, level());
+		}
+
+		// 处理光源方块删除
+		if (lightRemovalTimer > 0) {
+			lightRemovalTimer--;
+			if (lightRemovalTimer == 0) {
+				// 删除光源方块
+				TurretLightUtil.handleTurretFireLight(this, level(), 0); // 传入0表示停止开火
+			}
+		}
+	}
+
+	@Override
+	public void vehicleShoot(net.minecraft.world.entity.LivingEntity living, java.util.UUID uuid, net.minecraft.world.phys.Vec3 targetPos) {
+		super.vehicleShoot(living, uuid, targetPos);
+		// 处理炮口火光效果
+		TurretLightUtil.handleTurretFireLight(this, level(), 1); // 传入1表示正在开火
+		// 设置删除计时器
+		lightRemovalTimer = 1; // 1 tick后删除
+	}
+	@Override
+	public ResourceLocation getNightVisionShader() {
+		return new ResourceLocation("shaders/post/night-vision-wp.json");
+	}
 }
