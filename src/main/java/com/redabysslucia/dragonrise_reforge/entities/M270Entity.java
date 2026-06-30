236b5f333c7b0136e99cc5d6d735ca23ee540ef5
Ly01_lin <863716833@qq.com>
2026-06-30 18:10:20 +0800

    武器数值重写

--- src/main/java/com/redabysslucia/dragonrise_reforge/entities/M270Entity.java
+++ src/main/java/com/redabysslucia/dragonrise_reforge/entities/M270Entity.java
@@ -1,16 +1,45 @@
 package com.redabysslucia.dragonrise_reforge.entities;
 
 import com.atsuishio.superbwarfare.entity.vehicle.base.GeoVehicleEntity;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.UUID;
 
 @SuppressWarnings("removal")
 public class M270Entity extends GeoVehicleEntity {
 
-        public M270Entity(EntityType<M270Entity> type, Level world) {
-                super(type, world);
+    public M270Entity(EntityType<M270Entity> type, Level world) {
+        super(type, world);
+    }
+
+    private boolean isAngleRestricted() {
+        float turretYaw = getTurretYRot();
+        float turretPitch = getTurretXRot();
+        return Mth.abs(turretYaw) <= 50f && -turretPitch < 30f;
+    }
+
+    @Override
+    public boolean canShoot(LivingEntity living) {
+        if (isAngleRestricted()) {
+            return false;
         }
+        return super.canShoot(living);
+    }
 
-
+    @Override
+    public void vehicleShoot(LivingEntity living, UUID uuid, Vec3 targetPos) {
+        if (isAngleRestricted()) {
+            if (living instanceof Player player) {
+                player.displayClientMessage(Component.translatable("message.dragonrise_reforge.m270_angle_restricted"), true);
+            }
+            return;
+        }
+        super.vehicleShoot(living, uuid, targetPos);
+    }
 }
