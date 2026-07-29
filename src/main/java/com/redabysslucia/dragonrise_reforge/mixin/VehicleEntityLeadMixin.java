816c05ede6761ca9864478366f4c28704abc50be
Lg <16448113+lgen141@user.noreply.gitee.com>
2026-07-29 18:32:16 +0800

    自动化

--- src/main/java/com/redabysslucia/dragonrise_reforge/mixin/VehicleEntityLeadMixin.java
+++ src/main/java/com/redabysslucia/dragonrise_reforge/mixin/VehicleEntityLeadMixin.java
@@ -25,8 +25,8 @@ import java.util.UUID;
 @Mixin(value = VehicleEntity.class)
 public abstract class VehicleEntityLeadMixin {
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void onInit(CallbackInfo ci) {
+    @Inject(method = "defineSynchedData", at = @At("RETURN"))
+    private void onDefineSynchedData(CallbackInfo ci) {
         var data = ((VehicleEntity) (Object) this).getEntityData();
         data.define(VehicleLeashData.LEASH_HOLDER, Optional.empty());
         data.define(VehicleLeashData.CHAIN_LEASHED, false);
