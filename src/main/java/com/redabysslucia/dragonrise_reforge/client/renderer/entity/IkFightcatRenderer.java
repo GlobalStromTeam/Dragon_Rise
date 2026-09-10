3617cbf297c57c1856f80106a03f34f91044374c
Ly01_lin <863716833@qq.com>
2026-09-10 21:02:33 +0800

    部分新音道

---
+++ src/main/java/com/redabysslucia/dragonrise_reforge/client/renderer/entity/IkFightcatRenderer.java
@@ -0,0 +1,11 @@
+package com.redabysslucia.dragonrise_reforge.client.renderer.entity;
+
+import com.atsuishio.superbwarfare.client.renderer.entity.GeoVehicleRenderer;
+import com.redabysslucia.dragonrise_reforge.entities.IkFightcatEntity;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+
+public class IkFightcatRenderer extends GeoVehicleRenderer<IkFightcatEntity> {
+    public IkFightcatRenderer(EntityRendererProvider.Context renderManager) {
+        super(renderManager);
+    }
+}
